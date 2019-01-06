6003c7c1c1e694a13973170d3fc2b6760b577c50
Patrick Gray <pgray9189@gmail.com>
2019-01-06 01:09:47 -0500

    flake8 neural init

--- voicesep/separators/neural/__init__.py
+++ voicesep/separators/neural/__init__.py
@@ -1,2 +1,5 @@
-from voicesep.separators.neural import chord_level
-from voicesep.separators.neural import note_level
+from voicesep.separators.neural.dataset import Dataset
+from voicesep.separators.neural.network import Network
+from voicesep.separators.neural.note_level import NoteLevel
+
+__all__ = ["Dataset", "Network", "NoteLevel"]
