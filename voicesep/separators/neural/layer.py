a10d5e7dec002c17f30fe096269f9f2b45cd90be
Patrick Gray <pgray9189@gmail.com>
2019-01-06 01:23:04 -0500

    flake8 layer

--- voicesep/separators/neural/layer.py
+++ voicesep/separators/neural/layer.py
@@ -1,28 +1,39 @@
 import logging
+import numpy as np
+import theano
+import theano.tensor as T
+
+from voicesep.separation.neural import activations
 
 logger = logging.getLogger(__name__)
 
+
 class Layer:
 
-    def __init__(self, X_var, dimensions):
+    def __init__(self, X_var, input_size, output_size, activation):
 
-        assert len(dimensions) == 2
+        logger.debug(
+            "input_size={}, output_size={} | initializing"
+            .format(input_size, output_size)
+        )
 
-        self.dimensions = dimensions
+        self.dimensions = (input_size, output_size)
 
-        W = np.empty(dimensions, dtype=theano.config.floatX)
-        b = np.empty(dimensions[1:], dtype=theano.config.floatX)
+        W = np.empty(self.dimensions, dtype=theano.config.floatX)
+        b = np.empty(self.dimensions[1:], dtype=theano.config.floatX)
 
         self.W_var = theano.shared(value=W, name="W", borrow=True)
         self.b_var = theano.shared(value=b, name="b", borrow=True)
 
-        self.params = [self.W_var, self.b_var]
+        self.params = (self.W_var, self.b_var)
 
-        self.y_hat_var = activation(T.dot(X_var, self.W_var) + self.b_var)
+        self.y_hat_var = getattr(activation, activations)(
+            T.dot(X_var, self.W_var) + self.b_var
+        )
 
     def random_weights(self):
 
-        random_num = np.sqrt(6 / sum(dimensions))
+        random_num = np.sqrt(6.0 / sum(self.dimensions))
 
         W = np.asarray(
             np.random.uniform(
@@ -35,9 +46,6 @@ class Layer:
         self.set_weights(W, b)
 
     def set_weights(self, W, b):
-
-        assert W.shape == self.W_var.get_value(borrow=True).shape
-        assert b.shape == self.b_var.get_value(borrow=True).shape
 
         self.W_var.set_value(W, borrow=True)
         self.b_var.set_value(b, borrow=True)
