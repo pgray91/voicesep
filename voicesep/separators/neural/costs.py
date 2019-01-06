67cba55da778b71af4421603ff40b449bf286844
Patrick Gray <pgray9189@gmail.com>
2019-01-06 00:14:39 -0500

    flake8 costs

--- voicesep/separators/neural/costs.py
+++ voicesep/separators/neural/costs.py
@@ -1,6 +1,6 @@
-import theano
 import theano.tensor as T
 
-def binary_crossentropy(y_var, y_hat_var):
+
+def binary_crossentropy(y_hat_var, y_var):
 
     return T.nnet.binary_crossentropy(y_hat_var, y_var).mean()
