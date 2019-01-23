4db0c10cb9392415b63e0956c8e2bbaa3f9cf7c9
Patrick Gray <pgray9189@gmail.com>
2019-01-23 01:10:39 -0500

    Checkpoint

--- voicesep/separators/neural/chord_level/network/__init__.py
+++ voicesep/separators/neural/chord_level/network/__init__.py
@@ -26,33 +26,50 @@ class Network:
         self.dimensions = ()
         self.hidden_activations = None
         self.output_activation = None
-        self.layers = []
+        self.convolutional_layers = []
+        self.simple_layers = []
 
         self.train_function = None
         self.predict_function = None
 
-    def build_convolution(self, count, dimensions, vector_count, activation):
-
-        X_conv_vars = [T.matrix("X_conv{}".format(i + 1) for i in range(count))]
-        convolution_layers = [
-            layers.Convolutional(
-                X_conv_vars[i], *dimensions, vector_count, activation
-            )
-            for i in range(count)
-        ]
-
     def build(
         self,
+        convolutional_dimensions,
+        convolutional_counts,
         dimensions,
         hidden_activations,
         output_activation
     ):
+        self.X_var = T.matrix("X", dtype=theano.config.floatX)
+        self.y_var = T.vector("y", dtype=theano.config.floatX)
+
+        self.X_conv_vars = []
+        # just use self.layers
+        self.convolutional_layers = []
+        for i, convolutional_dimension, convolutional_count in enumerate(zip(convolutional_dimensions, convolutional_counts)):
+
+            self.X_conv_vars.append(T.matrix("X_conv{}".format(i + 1)))
+
+            self.convolutional_layers.append(
+                Layer(
+                    X_conv_vars[i], *convolutional_dimension, hidden_activations
+                )
+            )
+
+            y_hat_var = self.convolutional_layers[-1].y_hat_var
+            y_hat_var = (
+                y_hat_var.reshape(
+                    (y_hat_var.shape[0] // convolutional_count, convolutional_count, output_size)
+                )
+                .argmax(axis=1)
+            )
+
+            dimensions[0] += convolutional_dimension[1]
+            self.X_var = T.concatenate([self.X_var, y_hat_var], axis=1)
 
         self.dimensions = dimensions
         self.hidden_activations = hidden_activations
         self.output_activation = output_activation
-        self.convolutional_layers = []
-        self.simple_layers = []
 
         next_input = self.X_var
         for i in range(len(dimensions) - 2):
@@ -75,12 +92,12 @@ class Network:
             )
         )
 
-    def compile(self, cost_type, L2_reg, gradient_type, gradient_args):
+    def compile(self, cost_type, cost_args, L2_reg, gradient_type, gradient_args):
 
         y_hat_var = self.layers[-1].y_hat_var.flatten()
 
         cost_function = getattr(costs, cost_type)
-        cost_var = cost_function(self.y_var, y_hat_var)
+        cost_var = cost_function(y_hat_var, *cost_args)
         cost_var += L2_reg * sum(
             T.sum(T.pow(layer.W_var, 2))
             for layer in self.layers
@@ -95,13 +112,13 @@ class Network:
         updates = gradient_function(params, cost_var, *gradient_args)
 
         self.train_function = theano.function(
-            inputs=[self.X_var, self.y_var],
+            inputs=self.X_vars,
             outputs=cost_var,
             updates=updates
         )
 
         self.predict_function = theano.function(
-          inputs=[self.X_var],
+          inputs=self.X_vars,
           outputs=y_hat_var
         )
 
@@ -116,9 +133,9 @@ class Network:
         for epoch in range(1, epochs + 1):
             cost = 0
             for i in range(batch_count):
-                X_batch, y_batch = dataset[i * batch_size: (i + 1) * batch_size]
+                batches = dataset[i * batch_size: (i + 1) * batch_size]
 
-                cost += self.train_function(X_batch, y_batch)
+                cost += self.train_function(*batches)
 
             if verbosity and int(time.time() - checkpoint) > verbosity:
                 logger.info(
