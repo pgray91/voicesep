dd7830c23cf3c20bc61182062f5245d6504b553d
Patrick Gray <scythedman69@laptop.localdomain>
2019-11-23 20:37:38 -0500

    benchmark *

--- tests/separators/benchmark/__init__.py
+++ tests/separators/benchmark/__init__.py
@@ -13,7 +13,11 @@ class Test(unittest.TestCase):
         name = self._testMethodName
         sheet = f"{path}/{name}.musicxml"
 
-        score = vs.Score(sheet)
+        self.score = vs.Score(sheet)
+        self.beat_horizon = 2
+
+    def test_one_to_many(self):
+
         waterfall = [
             {
                 "Benchmark": {
@@ -21,24 +25,21 @@ class Test(unittest.TestCase):
                 }
             }
         ]
-        beat_horizon = 2
 
-        assignments = vs.separate(score, waterfall, beat_horizon)
-        self.voices = [voice for assignment in assignments for voice in assignment]
-
-    def test_one_to_many(self):
+        assignments = vs.separate(self.score, waterfall, self.beat_horizon)
+        voices = [voice for assignment in assignments for voice in assignment]
 
         expected_pairs = [
-            (self.voices[0], self.voices[2]),
-            (self.voices[0], self.voices[3]),
-            (self.voices[1], self.voices[3]),
-            (self.voices[1], self.voices[4]),
-            (self.voices[2], self.voices[6]),
-            (self.voices[2], self.voices[7]),
-            (self.voices[3], self.voices[8]),
-            (self.voices[4], self.voices[8]),
-            (self.voices[6], self.voices[9]),
-            (self.voices[7], self.voices[10])
+            (voices[0], voices[2]),
+            (voices[0], voices[3]),
+            (voices[1], voices[3]),
+            (voices[1], voices[4]),
+            (voices[2], voices[6]),
+            (voices[2], voices[7]),
+            (voices[3], voices[8]),
+            (voices[4], voices[8]),
+            (voices[6], voices[9]),
+            (voices[7], voices[10])
         ]
 
         for pair in expected_pairs:
@@ -47,9 +48,20 @@ class Test(unittest.TestCase):
 
     def test_one_to_many_convergence(self):
 
+        waterfall = [
+            {
+                "Benchmark": {
+                    "one_to_many": True
+                }
+            }
+        ]
+
+        assignments = vs.separate(self.score, waterfall, self.beat_horizon)
+        voices = [voice for assignment in assignments for voice in assignment]
+
         expected_pairs = [
-            (self.voices[0], self.voices[2]),
-            (self.voices[1], self.voices[2])
+            (voices[0], voices[2]),
+            (voices[1], voices[2])
         ]
 
         for pair in expected_pairs:
@@ -58,9 +70,20 @@ class Test(unittest.TestCase):
 
     def test_one_to_many_divergence(self):
 
+        waterfall = [
+            {
+                "Benchmark": {
+                    "one_to_many": True
+                }
+            }
+        ]
+
+        assignments = vs.separate(self.score, waterfall, self.beat_horizon)
+        voices = [voice for assignment in assignments for voice in assignment]
+
         expected_pairs = [
-            (self.voices[0], self.voices[1]),
-            (self.voices[0], self.voices[2])
+            (voices[0], voices[1]),
+            (voices[0], voices[2])
         ]
 
         for pair in expected_pairs:
@@ -69,18 +92,55 @@ class Test(unittest.TestCase):
 
     def test_one_to_one(self):
 
+        waterfall = [
+            {
+                "Benchmark": {
+                    "one_to_many": False
+                }
+            }
+        ]
+
+        assignments = vs.separate(self.score, waterfall, self.beat_horizon)
+        voices = [voice for assignment in assignments for voice in assignment]
+
         expected_pairs = [
-            (self.voices[0], self.voices[2]),
-            (self.voices[1], self.voices[3]),
-            (self.voices[2], self.voices[6]),
-            (self.voices[3], self.voices[8]),
-            (self.voices[6], self.voices[9]),
-            (self.voices[7], self.voices[10])
+            (voices[0], voices[2]),
+            (voices[1], voices[3]),
+            (voices[2], voices[6]),
+            (voices[3], voices[8]),
+            (voices[6], voices[9]),
+            (voices[7], voices[10])
         ]
 
         for pair in expected_pairs:
             with self.subTest(f"({pair[0]}, {pair[1]})"):
                 self.assertTrue(pair[1] in pair[0].right)
+
+    def test_one_to_many_clear(self):
+
+        waterfall = [
+            {
+                "Benchmark": {
+                    "one_to_many": True
+                }
+            }
+        ]
+
+        assignments = vs.separate(self.score, waterfall, self.beat_horizon)
+        voices = [voice for assignment in assignments for voice in assignment]
+
+        expected_pairs = [
+            (voices[0], voices[1]),
+            (voices[1], voices[2]),
+            (voices[2], voices[3]),
+            (voices[2], voices[4])
+        ]
+
+        for pair in expected_pairs:
+            with self.subTest(f"({pair[0]}, {pair[1]})"):
+                self.assertTrue(pair[1] in pair[0].right)
+
+        self.assertFalse(voices[0] in voices[2].left)
 
 
 if __name__ == "__main__":
